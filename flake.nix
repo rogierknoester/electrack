0e6f345879b462df8b1cd3400e136606312a0ee3
rogierknoester <dev@rogierknoester.nl>
2025-03-23 19:30:02 +0100

    chore(dev): update dev env

--- flake.nix
+++ flake.nix
@@ -2,20 +2,42 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { self, nixpkgs }:
-
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
     {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustc
+            rustfmt
+            clippy
+            cargo
+            rust-analyzer
+            pkg-config
+          ];
+
+          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+        };
+      }
+    )
+    // {
       nixosModules = {
         electrack = import ./electrack-module.nix;
       };
+
     };
 
 }
