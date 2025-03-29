ef06847bfdfa487263ccada23e91525bad47e5d1
rogierknoester <dev@rogierknoester.nl>
2025-03-29 15:09:43 +0100

    chore(dev): use rust overlay

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs =
@@ -10,23 +11,27 @@
       self,
       nixpkgs,
       flake-utils,
+      rust-overlay,
     }:
 
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
       in
       {
 
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
-            rustc
-            rustfmt
-            clippy
-            cargo
-            rust-analyzer
             pkg-config
+
+            (rust-bin.stable.latest.default.override {
+              targets = [
+                "x86_64-unknown-linux-gnu"
+                "x86_64-unknown-linux-musl"
+              ];
+            })
           ];
 
           PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
